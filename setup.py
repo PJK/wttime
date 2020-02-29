82882c65e1263042fc67c12cbb0ba8dba1289e16
Pavel Kalvoda <me@pavelkalvoda.com>
2020-02-29 21:19:45 +0100

    Minor version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name='wttime',
-      version='0.1',
+      version='0.1.1',
       description='Smart timestamp utility',
       long_description=long_description,
       long_description_content_type='text/markdown',
